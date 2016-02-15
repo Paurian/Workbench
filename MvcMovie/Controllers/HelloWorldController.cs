4e169be801b294eee3dd371e349b2e67e23c3d97
Paurian <developer@paurian.com>
2016-02-15 11:50:55 -0700

    Initial CheckIn

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Greeting = "Hello, ";
+            ViewBag.Person = name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
